2f209d0d764a3595b6315cad5b4b6811ea91a9c8
Manoj Kumar <ms8460149@gmail.com>
2025-05-27 19:54:28 +0530

    fix: Build error

--- src/app/view/page.tsx
+++ src/app/view/page.tsx
@@ -3,5 +3,6 @@ import { fetchEventDetails } from "../../lib/event";
 
 export default async function ViewEventPage() {
   const event = await fetchEventDetails();
+  if(!event) return <div>Event not found</div>
   return <EventInvite event={event} />;
 }
